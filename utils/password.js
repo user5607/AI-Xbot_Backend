c1f4aaa7223d22dd1ef061ad624ed55303ec3e07
user5607 <3161769691@qq.com>
2025-12-10 19:01:25 +0800

    fix: 12.10_10:35

--- utils/password.js
+++ utils/password.js
@@ -1,10 +1,10 @@
 import bcrypt from 'bcryptjs';
 
-export function hashPassword(password) {
-  const salt = bcrypt.genSaltSync(10);
-  return bcrypt.hashSync(password, salt);
+export async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
 }
 
-export function verifyPassword(password, hashedPassword) {
-  return bcrypt.compareSync(password, hashedPassword);
+export async function verifyPassword(password, hashedPassword) {
+  return await bcrypt.compare(password, hashedPassword);
 }
